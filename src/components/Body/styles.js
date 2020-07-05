c59ac6323cad6a6c3a456f62c18f975e4293e9fc
Kayne G.I <kayne.goncalves@healthbit.com.br>
2020-07-05 04:59:48 -0300

    Estilizaçao do Body component

---
+++ src/components/Body/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding-left: 1rem;
+    padding-right: 1rem;
+    max-width: 100%;
+    border-radius: 30px;
+    padding: 0px 22px;
+    min-height: 80vh;
+    background: #D2DFEA;
+  display: flex;
+  align-items: center;
+  margin: 30px;
+`;
